2ead0393c61de3a08d1db6e65ba31dbc1dc7a641
Rajan <ganeshbabu.rajan@cgi.com>
2018-10-07 05:34:17 +0200

    (add) : local references added

--- src/app/add-server/add-server.component.ts
+++ src/app/add-server/add-server.component.ts
@@ -11,21 +11,21 @@ export class AddServerComponent implements OnInit {
   @Output() onServerCreated = new EventEmitter<ServerElement>();
   @Output() onBluePrintCreated = new EventEmitter<ServerElement>();
   
-  name : string = '';
-  content : string = '';
+  //name : string = '';
+  //content : string = '';
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  onAddServer(){
-    this.onServerCreated.emit(new ServerElement(this.name, this.content,'server'))
+  onAddServer(serverNameInput : HTMLInputElement, serverContentInput : HTMLInputElement){
+    this.onServerCreated.emit(new ServerElement(serverNameInput.value,serverContentInput.value,'server'))
     //this.serverElements.push(new ServerElement(this.name,this.content,'server'));
   }
 
-  onAddBluePrint(){
-    this.onBluePrintCreated.emit(new ServerElement(this.name,this.content,'blueprint'))
+  onAddBluePrint(serverNameInput : HTMLInputElement, serverContentInput : HTMLInputElement){
+    this.onBluePrintCreated.emit(new ServerElement(serverNameInput.value, serverContentInput.value,'blueprint'))
     //this.serverElements.push(new ServerElement(this.name,this.content,'blueprint'))
   }
 
